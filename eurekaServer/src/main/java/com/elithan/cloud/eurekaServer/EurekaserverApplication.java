005bdccfc817f383dbf90d7a35778c06a1d42bf4
unknown <elithan@163.com>
2017-11-21 22:42:04 +0800

    chapter1 and 2 check in

---
+++ eurekaServer/src/main/java/com/elithan/cloud/eurekaServer/EurekaserverApplication.java
@@ -0,0 +1,18 @@
+package com.elithan.cloud.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+}
---
+++ serviceRibbon/src/main/java/com/elithan/cloud/serviceRibbon/ServiceRibbonApplication.java
@@ -0,0 +1,28 @@
+package com.elithan.cloud.serviceRibbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
